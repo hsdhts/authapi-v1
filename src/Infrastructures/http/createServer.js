1174b3c9b26bbdb87563ff6e817bd47bcc71a64e
husada20 <husadahutasoit@gmail.com>
2024-04-10 15:32:48 +0700

    pass say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -27,7 +27,7 @@ const createServer = async (container) => {
 
   server.route({
     method: 'GET',
-    path: '/halo',
+    path: '/hello',
     handler: () => ({
       value: 'Hello world!',
     }),
